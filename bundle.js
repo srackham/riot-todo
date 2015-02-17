0f179caaca288d6878fd55d0596dc833299dd194
Stuart Rackham <srackham@gmail.com>
2015-02-18 08:08:20 +1300

    Rebuild Github Pages.

--- bundle.js
+++ bundle.js
@@ -53,11 +53,11 @@
 	
 	var riot = _interopRequire(__webpack_require__(4));
 	
-	var TodoStore = _interopRequire(__webpack_require__(2));
+	var TodoStore = _interopRequire(__webpack_require__(1));
 	
-	var dispatcher = _interopRequire(__webpack_require__(3));
+	var dispatcher = _interopRequire(__webpack_require__(2));
 	
-	__webpack_require__(1);
+	__webpack_require__(3);
 	
 	var todoStore = new TodoStore(dispatcher);
 	dispatcher.addStore(todoStore);
@@ -65,59 +65,6 @@
 
 /***/ },
 /* 1 */
-/***/ function(module, exports, __webpack_require__) {
-
-	"use strict";
-	
-	var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
-	
-	var riot = _interopRequire(__webpack_require__(4));
-	
-	riot.tag("todo-app", "<h3>Todos</h3>\n   <todo-form store=\"{opts.store}\"></todo-form>\n   <todo-list store=\"{opts.store}\"></todo-list>\n   <p>\n     Want a second fully synchronized list? Just declare another list component:\n     no code required, no events to wire up!\n   </p>\n   <todo-list store=\"{opts.store}\"></todo-list>", function (opts) {
-	  var dispatcher = this.opts.store.dispatcher;
-	  this.on("mount", function () {
-	    return dispatcher.trigger(dispatcher.INIT_TODOS);
-	  });
-	});
-	
-	
-	riot.tag("todo-form", "<form onsubmit=\"{add}\">\n     <input name=\"input\" type=\"text\" placeholder=\"New Todo\" autofocus=\"true\">\n     <input type=\"submit\" value=\"Add Todo\">\n   </form>\n   <button onclick=\"{clear}\">Clear Completed</button>", function (opts) {
-	  var _this = this;
-	  var store = this.opts.store;
-	  var dispatcher = store.dispatcher;
-	
-	  this.add = function (e) {
-	    if (_this.input.value) {
-	      dispatcher.trigger(dispatcher.ADD_TODO, { title: _this.input.value, done: false });
-	      _this.input.value = "";
-	    }
-	  };
-	
-	  this.clear = function (e) {
-	    dispatcher.trigger(dispatcher.CLEAR_TODOS);
-	  };
-	});
-	
-	
-	riot.tag("todo-list", "<ul>\n     <li each=\"{todo in opts.store.todos}\">\n       <todo-item store=\"{parent.opts.store}\" todo=\"{todo}\">\n     </li>\n   </ul>", function (opts) {
-	  var _this = this;
-	  var store = this.opts.store;
-	  store.on(store.CHANGED_EVENT, function () {
-	    return _this.update();
-	  });
-	});
-	
-	
-	riot.tag("todo-item", "<span class=\"{done: opts.todo.done}\" onclick=\"{toggle}\">\n     {opts.todo.title}\n   </span>", function (opts) {
-	  var dispatcher = this.opts.store.dispatcher;
-	
-	  this.toggle = function () {
-	    dispatcher.trigger(dispatcher.TOGGLE_TODO, opts.todo);
-	  };
-	});
-
-/***/ },
-/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// TodoStore definition.
@@ -171,7 +118,7 @@
 	}
 
 /***/ },
-/* 3 */
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
@@ -222,14 +169,67 @@
 	};
 
 /***/ },
+/* 3 */
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
+	
+	var riot = _interopRequire(__webpack_require__(4));
+	
+	riot.tag("todo-app", "<h3>Todos</h3>\n   <todo-form store=\"{opts.store}\"></todo-form>\n   <todo-list store=\"{opts.store}\"></todo-list>\n   <p>\n     Want a second fully synchronized list? Just declare another list component:\n     no code required, no events to wire up!\n   </p>\n   <todo-list store=\"{opts.store}\"></todo-list>", function (opts) {
+	  var dispatcher = this.opts.store.dispatcher;
+	  this.on("mount", function () {
+	    return dispatcher.trigger(dispatcher.INIT_TODOS);
+	  });
+	});
+	
+	
+	riot.tag("todo-form", "<form onsubmit=\"{add}\">\n     <input name=\"input\" type=\"text\" placeholder=\"New Todo\" autofocus=\"true\">\n     <input type=\"submit\" value=\"Add Todo\">\n   </form>\n   <button onclick=\"{clear}\">Clear Completed</button>", function (opts) {
+	  var _this = this;
+	  var store = this.opts.store;
+	  var dispatcher = store.dispatcher;
+	
+	  this.add = function (e) {
+	    if (_this.input.value) {
+	      dispatcher.trigger(dispatcher.ADD_TODO, { title: _this.input.value, done: false });
+	      _this.input.value = "";
+	    }
+	  };
+	
+	  this.clear = function (e) {
+	    dispatcher.trigger(dispatcher.CLEAR_TODOS);
+	  };
+	});
+	
+	
+	riot.tag("todo-list", "<ul>\n     <li each=\"{todo in opts.store.todos}\">\n       <todo-item store=\"{parent.opts.store}\" todo=\"{todo}\">\n     </li>\n   </ul>", function (opts) {
+	  var _this = this;
+	  var store = this.opts.store;
+	  store.on(store.CHANGED_EVENT, function () {
+	    return _this.update();
+	  });
+	});
+	
+	
+	riot.tag("todo-item", "<span class=\"{done: opts.todo.done}\" onclick=\"{toggle}\">\n     {opts.todo.title}\n   </span>", function (opts) {
+	  var dispatcher = this.opts.store.dispatcher;
+	
+	  this.toggle = function () {
+	    dispatcher.trigger(dispatcher.TOGGLE_TODO, opts.todo);
+	  };
+	});
+
+/***/ },
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
-	/* Riot v2.0.8, @license MIT, (c) 2015 Muut Inc. + contributors */
+	/* Riot v2.0.9, @license MIT, (c) 2015 Muut Inc. + contributors */
 	
 	;(function() {
 	
-	var riot = { version: 'v2.0.8', settings: {} }
+	var riot = { version: 'v2.0.9', settings: {} }
 	
 	'use strict'
 	
@@ -237,10 +237,13 @@
 	
 	  el = el || {}
 	
-	  var callbacks = {}
+	  var callbacks = {},
+	      _id = 0
 	
 	  el.on = function(events, fn) {
 	    if (typeof fn == 'function') {
+	      fn._id = _id++
+	
 	      events.replace(/\S+/g, function(name, pos) {
 	        (callbacks[name] = callbacks[name] || []).push(fn)
 	        fn.typed = pos > 0
@@ -254,7 +257,7 @@
 	    else if (fn) {
 	      var arr = callbacks[events]
 	      for (var i = 0, cb; (cb = arr && arr[i]); ++i) {
-	        if (cb == fn) { arr.splice(i, 1); i-- }
+	        if (cb._id == fn._id) { arr.splice(i, 1); i-- }
 	      }
 	    } else {
 	      events.replace(/\S+/g, function(name) {
@@ -297,8 +300,8 @@
 	
 	  var loc = location,
 	      fns = riot.observable(),
-	      current = hash(),
-	      win = window
+	      win = window,
+	      current
 	
 	  function hash() {
 	    return loc.hash.slice(1)
@@ -402,7 +405,7 @@
 	
 	    // make sure we use current brackets setting
 	    var b = riot.settings.brackets || '{ }'
-	    if(b != brackets){
+	    if (b != brackets) {
 	      brackets = b.split(' ')
 	      re_expr = re(/({[\s\S]*?})/)
 	    }
@@ -426,7 +429,7 @@
 	      // temporarily convert \{ and \} to a non-character
 	      .replace(re(/\\{/), '\uFFF0')
 	      .replace(re(/\\}/), '\uFFF1')
-	      
+	
 	      // split string to expression and non-expresion parts
 	      .split(re_expr)
 	
@@ -482,14 +485,14 @@
 	      .replace(re(/^[{ ]+|[ }]+$|\/\*.+?\*\//g), '')
 	
 	    // is it an object literal? i.e. { key : value }
-	    return /^\s*[\w-"']+ *:/.test(s)
+	    return /^\s*[\w- "']+ *:/.test(s)
 	
 	      // if object literal, return trueish keys
 	      // e.g.: { show: isOpen(), done: item.done } -> "show done"
-	      ? '[' + s.replace(/\W*([\w-]+)\W*:([^,]+)/g, function(_, k, v) {
+	      ? '[' + s.replace(/\W*([\w- ]+)\W*:([^,]+)/g, function(_, k, v) {
 	
 	          // safely execute vars to prevent undefined value errors
-	          return v.replace(/\w[^,|& ]*/g, function(v) { return wrap(v, n) }) + '?"' + k + '":"",'
+	          return v.replace(/\w[^,|& ]*/g, function(v) { return wrap(v, n) }) + '?"' + k.trim() + '":"",'
 	
 	        }) + '].join(" ")'
 	
@@ -535,12 +538,20 @@
 	      els = expr.split(/\s+in\s+/)
 	
 	  if (els[1]) {
-	    ret.val = '{ ' + els[1]
-	    els = els[0].slice(1).trim().split(/,\s*/)
+	    ret.val = expr_begin + els[1]
+	    els = els[0].slice(expr_begin.length).trim().split(/,\s*/)
 	    ret.key = els[0]
 	    ret.pos = els[1]
 	  }
+	
 	  return ret
+	}
+	
+	function mkitem(expr, key, val) {
+	  var item = {}
+	  item[expr.key] = key
+	  if (expr.pos) item[expr.pos] = val
+	  return item
 	}
 	
 	function _each(dom, parent, expr) {
@@ -561,7 +572,7 @@
 	    root.removeChild(dom)
 	
 	  }).one('mount', function() {
-	    if (!hasParent(root)) root = parent.root
+	    if (root.stub) root = parent.root
 	
 	  }).on('update', function() {
 	
@@ -575,17 +586,11 @@
 	      checksum = testsum
 	
 	      // clear old items
-	      tags.map(function(tag) {
-	        tag.unmount()
-	      })
-	
+	      tags.map(function(tag) { tag.unmount() })
 	      tags = rendered = []
 	
-	      items = Object.keys(items).map(function(key, i) {
-	        var obj = {}
-	        obj[expr.key] = key
-	        obj[expr.pos] = items[key]
-	        return obj
+	      items = Object.keys(items).map(function(key) {
+	        return mkitem(expr, key, items[key])
 	      })
 	
 	    }
@@ -600,22 +605,18 @@
 	        rendered.splice(pos, 1)
 	        tags.splice(pos, 1)
 	      }
+	
 	    })
 	
 	    // mount new
 	    var nodes = root.childNodes,
-	        prev_index = Array.prototype.indexOf.call(nodes, prev)
+	        prev_index = [].indexOf.call(nodes, prev)
 	
 	    arrDiff(items, rendered).map(function(item, i) {
 	
 	      var pos = items.indexOf(item)
 	
-	      if (!checksum && expr.key) {
-	        var obj = {}
-	        obj[expr.key] = item
-	        obj[expr.pos] = pos
-	        item = obj
-	      }
+	      if (!checksum && expr.key) item = mkitem(expr, item, pos)
 	
 	      var tag = new Tag({ tmpl: template }, {
 	        before: nodes[prev_index + 1 + pos],
@@ -646,10 +647,10 @@
 	
 	function parseLayout(root, tag, expressions) {
 	
-	  function addExpr(dom, value, data) {
-	    if (tmpl(value) || data) {
-	      var expr = { dom: dom, expr: value }
-	      expressions.push(extend(expr, data || {}))
+	  function addExpr(dom, val, extra) {
+	    if (val.indexOf(expr_begin) >= 0) {
+	      var expr = { dom: dom, expr: val }
+	      expressions.push(extend(expr, extra))
 	    }
 	  }
 	
@@ -667,13 +668,6 @@
 	    var attr = dom.getAttribute('each')
 	    if (attr) { _each(dom, tag, attr); return false }
 	
-	    // child tag
-	    var impl = tag_impl[dom.tagName.toLowerCase()]
-	    if (impl) {
-	      impl = new Tag(impl, { root: dom, parent: tag })
-	      return false
-	    }
-	
 	    // attributes
 	    each(dom.attributes, function(attr) {
 	      var name = attr.name,
@@ -689,6 +683,10 @@
 	      }
 	
 	    })
+	
+	    // child tag
+	    var impl = tag_impl[dom.tagName.toLowerCase()]
+	    if (impl) impl = new Tag(impl, { root: dom, parent: tag })
 	
 	  })
 	
@@ -710,7 +708,7 @@
 	
 	  opts = opts || {}
 	
-	  extend(this, { parent: parent, root: root, opts: opts, children: [] })
+	  extend(this, { parent: parent, root: root, opts: opts })
 	  extend(this, item)
 	
 	
@@ -722,7 +720,7 @@
 	    attributes[name] = val
 	
 	    // remove dynamic attributes from node
-	    if (val.indexOf('{') >= 0) {
+	    if (val.indexOf(expr_begin) >= 0) {
 	      remAttr(root, name)
 	      return false
 	    }
@@ -738,8 +736,6 @@
 	  updateOpts()
 	
 	  // child
-	  parent && parent.children.push(this)
-	
 	  var dom = mkdom(impl.tmpl),
 	      loop_dom
 	
@@ -756,40 +752,27 @@
 	  }
 	
 	  this.unmount = function() {
+	    var el = is_loop ? loop_dom : root,
+	        p = el.parentNode
 	
-	    if (is_loop) {
-	      root.removeChild(loop_dom)
-	
-	    } else {
-	      var p = root.parentNode
-	      p && p.removeChild(root)
+	    if (p) {
+	      p.removeChild(el)
+	      self.trigger('unmount')
+	      parent && parent.off('update', self.update)
 	    }
-	
-	    // splice from parent.children[]
-	    if (parent) {
-	      var els = parent.children
-	      els.splice(els.indexOf(self), 1)
-	    }
-	
-	    self.trigger('unmount')
-	
-	    // cleanup
-	    parent && parent.off('update', self.update)
-	    mounted = false
 	  }
 	
 	  function mount() {
-	    while (dom.firstChild) {
-	      if (is_loop) {
-	        loop_dom = dom.firstChild
-	        root.insertBefore(dom.firstChild, conf.before || null) // null needed for IE8
 	
-	      } else {
-	        root.appendChild(dom.firstChild)
-	      }
+	    if (is_loop) {
+	      loop_dom = dom.firstChild
+	      root.insertBefore(loop_dom, conf.before || null) // null needed for IE8
+	
+	    } else {
+	      while (dom.firstChild) root.appendChild(dom.firstChild)
 	    }
 	
-	    if (!hasParent(root)) self.root = root = parent.root
+	    if (root.stub) self.root = root = parent.root
 	
 	    self.trigger('mount')
 	
@@ -865,7 +848,6 @@
 	
 	    // if- conditional
 	    } else if (attr_name == 'if') {
-	
 	      remAttr(dom, attr_name)
 	
 	      var stub = expr.stub
@@ -887,6 +869,9 @@
 	      dom.style.display = value ? '' : 'none'
 	
 	    // normal attribute
+	    } else if (attr_name == 'value') {
+	      dom.value = value
+	
 	    } else {
 	      if (expr.bool) {
 	        dom[attr_name] = value
@@ -919,7 +904,7 @@
 	
 	function mkdom(template) {
 	  var tag_name = template.trim().slice(1, 3).toLowerCase(),
-	      root_tag = /td|th/.test(tag_name) ? 'tr' : tag_name == 'tr' ? 'tbody' : 'div'
+	      root_tag = /td|th/.test(tag_name) ? 'tr' : tag_name == 'tr' ? 'tbody' : 'div',
 	      el = document.createElement(root_tag)
 	
 	  el.stub = true
@@ -942,31 +927,27 @@
 	  })
 	}
 	
-	// HTMLDocument == IE8 thing
-	function hasParent(el) {
-	  var p = el.parentNode,
-	      doc = window.HTMLDocument
-	
-	  return p && !(doc && p instanceof doc)
-	}
-	
 	/*
 	 Virtual dom is an array of custom tags on the document.
-	 Each tag stores an array of child tags.
 	 Updates and unmounts propagate downwards from parent to children.
 	*/
 	
 	var virtual_dom = [],
-	    tag_impl = {}
+	    tag_impl = {},
+	    expr_begin
 	
 	riot.tag = function(name, html, fn) {
+	  expr_begin = expr_begin || (riot.settings.brackets || '{ }').split(' ')[0]
 	  tag_impl[name] = { name: name, tmpl: html, fn: fn }
 	}
 	
 	var mountTo = riot.mountTo = function(root, tagName, opts) {
 	  var impl = tag_impl[tagName], tag
 	
-	  if (impl) tag = new Tag(impl, { root: root, opts: opts })
+	  if (impl && root) {
+	    root.riot = 0 // mountTo can override previous instance
+	    tag = new Tag(impl, { root: root, opts: opts })
+	  }
 	
 	  if (tag) {
 	    virtual_dom.push(tag)
